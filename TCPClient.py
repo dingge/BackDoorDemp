b6f74f97ee3289872f22e918f439cfc014cc7ea4
dinggege <529077434@qq.com>
2017-07-26 13:08:51 +0800

    添加Struct,增加TCP发送文件的功能。

--- TCPClient.py
+++ TCPClient.py
@@ -1,20 +1,25 @@
 # -*- coding: utf-8 -*-
-import socket
+import socket,struct,os
   
 # 目标地址IP/URL及端口
 target_host = "127.0.0.1"
-target_port = 9999
+target_port = 9998
   
 # 创建一个socket对象
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
   
 # 连接主机
 client.connect((target_host,target_port))
-  
-# 发送数据
-client.send("GET / HTTP/1.1\r\nHOST:127.0.0.1\r\n\r\n")
-  
-# 接收响应
-response = client.recv(4096)
-  
-print response
+
+FILEPATH = './TempSend/file.txt'
+FileInfo_size = struct.calcsize('128sl')
+fhead = struct.pack('128sl',os.path.basename(FILEPATH),os.stat(FILEPATH).st_size)
+client.send(fhead)
+fo = open(FILEPATH,'rb')
+
+while True:
+       FileData = fo.read(1024)
+       if not FileData:
+              break
+       client.send(FileData)
+fo.close()
--- TCPserver.py
+++ TCPserver.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import socket
 import threading
+import struct
+import os
   
 # 监听的IP及端口
 bind_ip = "127.0.0.1"
-bind_port = 9999
+bind_port = 9998
   
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
   
@@ -13,16 +15,32 @@ server.bind((bind_ip,bind_port))
 server.listen(5)
   
 print "[*] Listening on %s:%d" % (bind_ip,bind_port)
-  
+
 def handle_client(client_socket):
   
-    request = client_socket.recv(1024)
-  
-    print "[*] Received:%s" % request
-  
-    client_socket.send("ok!")
-  
-    client_socket.close()
+    #request = client_socket.recv(1024)
+    FileInfo = struct.calcsize('128sl')
+    print FileInfo
+    buf = client_socket.recv(FileInfo)
+    print len(buf)
+    if buf:
+        FileName,FileSize = struct.unpack('128sl',buf)
+        print 'FileName is :',FileName,'FileNameSize is :',len(FileName)
+        FILEWNAME=os.path.join(('./TempRec/'+FileName).strip('\00'))
+        print FILEWNAME,type(FILEWNAME)
+        recvd_size=0
+        file = open(FILEWNAME,'wb')
+        print 'start receiving...'
+        while not recvd_size == FileSize:
+            if FileSize - recvd_size > 1024:
+                rdata = client_socket.recv(1024)
+                recvd_size += len(rdata)
+            else:
+                rdata = client_socket.recv(FileSize - recvd_size)
+                recvd_size = FileSize
+            file.write(rdata)
+        file.close()
+        print 'Receive Done!'         
   
 while True:
   
